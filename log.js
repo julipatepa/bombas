bc1c4b3365a34578afb0dfb01c9a4fdaff50451d
mastergroup <juli_tepa@hotmail.com>
2024-12-12 00:25:20 -0300

    Primer commit

---
+++ log.js
@@ -0,0 +1,14 @@
+const accordionItems = document.querySelectorAll('.accordion-collapse');
+const mapImage = document.querySelector('img[alt="Mapa de ubicación"]');
+
+accordionItems.forEach(item => {
+    item.addEventListener('shown.bs.collapse', () => {
+        const accordionHeight = document.querySelector('.col-md-4').offsetHeight;
+        mapImage.style.height = `${accordionHeight}px`;
+    });
+
+    item.addEventListener('hidden.bs.collapse', () => {
+        const accordionHeight = document.querySelector('.col-md-4').offsetHeight;
+        mapImage.style.height = `${accordionHeight}px`;
+    });
+});
